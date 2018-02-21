3daada28fd20115a2479f196ad5cc902270c096f
loudsquelch <craig@loudsquel.ch>
2018-02-21 22:11:59 +0000

    Added pagination to GraphQL query

--- getGitHubRepos.py
+++ getGitHubRepos.py
@@ -1,4 +1,4 @@
-import re
+#import re
 import os
 import requests
 import simplejson as json
@@ -8,7 +8,7 @@ def getGitHubRepositoryInfo(apiToken):
   jsonQuery = """
   {
     viewer {
-      repositories(first: 5) {
+      repositories({{ repositoriesArgs }}) {
         totalCount
         pageInfo {
           hasNextPage
@@ -32,21 +32,23 @@ def getGitHubRepositoryInfo(apiToken):
   }
   """
   # Run initial query, limiting results to 5 (low enough to test code works as I only have 9 repos at the time of testing)
-  query = { 'query' : jsonQuery}
+  repositoriesArgs = 'first: 5'
+  query = { 'query' : jsonQuery.replace('{{ repositoriesArgs }}', repositoriesArgs)}
   headers = {'Authorization': 'token {0}'.format(apiToken)}
   queryResult = requests.post(url=url, json=query, headers=headers)
   results = json.loads(queryResult.text)
 
   repos = []
-  for repository in results['data']['viewer']['repositories']:
+  for repository in results['data']['viewer']['repositories']['edges']:
     repos.append(repository)
   # Load next page of results (if there are more than 5 in total)
   while results['data']['viewer']['repositories']['pageInfo']['hasNextPage'] == True:
     # This substitution is failing and we are getting stuck in an infiniteloop!!!!!!!
-    nextPageQuery = { 'query' : re.sub('repositories(first: 5)','repositories(first: 5, after: {0})'.format(results['data']['viewer']['repositories']['pageInfo']['endCursor']),jsonQuery)}
+    repositoriesArgs = 'first: 5, after: "' + results['data']['viewer']['repositories']['pageInfo']['endCursor'] + '"'
+    nextPageQuery = { 'query' : jsonQuery.replace('{{ repositoriesArgs }}', repositoriesArgs)}
     queryResult = requests.post(url=url, json=nextPageQuery, headers=headers)
     results = json.loads(queryResult.text)
-    for repository in results['data']['viewer']['repositories']:
+    for repository in results['data']['viewer']['repositories']['edges']:
       repos.append(repository)    
   
   return repos
@@ -71,13 +73,12 @@ htmlTable += '    <th>Name</th>\n'
 htmlTable += '    <th>Description</th>\n'
 htmlTable += '    <th>Updated</th>\n'
 htmlTable += '  </tr>\n'
-# print(j['data']['viewer']['repositories']['totalCount'])
-for edge in j['data']['viewer']['repositories']['edges']:
-  if len(edge['node']['releases']['nodes']) >= 1:
+for repository in j:
+  if len(repository['node']['releases']['nodes']) >= 1:
     htmlTable += '  <tr>\n'
-    htmlTable += '    <td><a href="{0}">{1}</a></td>\n'.format(edge['node']['url'], edge['node']['name'])
-    htmlTable += '    <td>{0}</td>\n'.format(edge['node']['description'])
-    htmlTable += '    <td>{0}/td>\n'.format(edge['node']['updatedAt'])
+    htmlTable += '    <td><a href="{0}">{1}</a></td>\n'.format(repository['node']['url'], repository['node']['name'])
+    htmlTable += '    <td>{0}</td>\n'.format(repository['node']['description'])
+    htmlTable += '    <td>{0}/td>\n'.format(repository['node']['updatedAt'])
     htmlTable += '  </tr>\n'
 
 htmlTable += '</table>\n'
